d2c3c1dc07563359d8a86ebd5b6292d1582655be
Luanda Brito <49211714+luandabrito@users.noreply.github.com>
2020-07-02 14:36:45 -0300

    fundamentos

---
+++ fundamentos/numeros_cuidados.js
@@ -0,0 +1,9 @@
+console.log(7 / 0)
+console.log('10' / 2)
+console.log('3' + 2) // concatenação e não soma, a preferência é da string
+console.log(2 + '3')
+console.log('Show!' * 2)
+console.log(0.1 + 0.7)
+// console.log(10.toString())
+console.log((10.3458).toFixed(2))
+console.log((0.1 + 0.7).toFixed(1))
